fa99b35394d1220fa1b8bb51a9f81eb2f91a8a6b
Denis Kataev <denis.a.kataev+git@gmail.com>
2022-05-04 10:09:53 +0300

    Add first working algo

---
+++ from_count.py
@@ -0,0 +1,49 @@
+"""
+Generate points from K different realizations
+
+Source:
+DISCRETE-EVENT SIMULATION: A FIRST COURSE
+L.Leemis & S.Park
+
+Algorithm 9.3.1
+"""
+import random
+
+import numpy as np
+from sparklines import sparklines
+
+# random.seed(42)
+
+data = np.array([
+    [1, 2, 4, 4, 6, 4, 3, 1, 1],  # seed=1
+    [1, 3, 2, 5, 6, 5, 6, 2, 0],  # seed=2
+    [0, 3, 1, 5, 7, 5, 4, 3, 0],  # seed=3
+])
+a = np.arange(0, 5, 0.5)
+n = data.sum(axis=0)
+
+m = 9
+k = 3
+
+lmb_max = sum(n / k)
+E = random.expovariate(1.0)
+T = []
+i = 1
+j = 0
+
+lmb = n[i - 1] / k
+
+while E <= lmb_max:
+    while E > lmb:
+        i += 1
+        lmb += n[i - 1] / k
+
+    T.append(a[i] - (lmb - E) * k * (a[i] - a[i - 1]) / n[i - 1])
+    E += random.expovariate(1.0)
+T.pop()
+
+events = [len([x for x in T if l < x <= r]) for l, r in zip(a, a[1:])]
+for i, x in enumerate(data):
+    print(f'or{i}', sparklines(x)[0], x, sum(x))
+print('gen', sparklines(events)[0], np.array(events), len(T))
+print(T)
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Modeling Nonstationary Process In Python
+More info https://stackoverflow.com/questions/40428009/which-books-are-used-today-to-learn-discrete-event-simulation
